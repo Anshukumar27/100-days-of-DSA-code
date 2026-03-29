642394f917c4d2e47613ec40ca7fa7c42bdb7539
Anshu kumar <anshu2005patna@gmail.com>
2026-03-29 13:56:29 +0530

    Add files via upload

---
+++ 057day.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int data) {
+    struct Node* n = (struct Node*)malloc(sizeof(struct Node));
+    n->data = data;
+    n->left = NULL;
+    n->right = NULL;
+    return n;
+}
+
+struct Node* queue[1000];
+int front = -1, rear = -1;
+
+void enqueue(struct Node* node) {
+    queue[++rear] = node;
+}
+
+struct Node* dequeue() {
+    return queue[++front];
+}
+
+struct Node* buildTree(int arr[], int n) {
+    if (n == 0 || arr[0] == -1)
+        return NULL;
+
+    struct Node* root = createNode(arr[0]);
+    enqueue(root);
+
+    int i = 1;
+
+    while (i < n) {
+        struct Node* curr = dequeue();
+
+       
+        if (arr[i] != -1) {
+            curr->left = createNode(arr[i]);
+            enqueue(curr->left);
+        }
+        i++;
+
+        if (i >= n) break;
+
+        if (arr[i] != -1) {
+            curr->right = createNode(arr[i]);
+            enqueue(curr->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+void mirror(struct Node* root) {
+    if (root == NULL)
+        return;
+
+    struct Node* temp = root->left;
+    root->left = root->right;
+    root->right = temp;
+
+    mirror(root->left);
+    mirror(root->right);
+}
+
+
+void inorder(struct Node* root) {
+    if (root == NULL)
+        return;
+
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    struct Node* root = buildTree(arr, n);
+
+    mirror(root);
+
+    inorder(root);
+
+    return 0;
+}
