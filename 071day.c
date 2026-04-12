7b0318361e4656f000332f1288570f3141ebaa13
Anshu kumar <anshu2005patna@gmail.com>
2026-04-12 08:58:37 +0530

    Add files via upload

---
+++ 071day.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <string.h>
+
+#define EMPTY -1
+
+int table[100]; 
+
+void insert(int key, int m) {
+    int h = key % m;
+
+    for (int i = 0; i < m; i++) {
+        int index = (h + i * i) % m;
+
+        if (table[index] == EMPTY) {
+            table[index] = key;
+            return;
+        }
+    }
+}
+
+void search(int key, int m) {
+    int h = key % m;
+
+    for (int i = 0; i < m; i++) {
+        int index = (h + i * i) % m;
+
+        if (table[index] == EMPTY) {
+            printf("NOT FOUND\n");
+            return;
+        }
+
+        if (table[index] == key) {
+            printf("FOUND\n");
+            return;
+        }
+    }
+
+    printf("NOT FOUND\n");
+}
+
+int main() {
+    int m, q;
+    scanf("%d", &m); 
+    scanf("%d", &q);  
+
+    for (int i = 0; i < m; i++) {
+        table[i] = EMPTY;
+    }
+
+    while (q--) {
+        char op[10];
+        int key;
+
+        scanf("%s %d", op, &key);
+
+        if (strcmp(op, "INSERT") == 0) {
+            insert(key, m);
+        } else if (strcmp(op, "SEARCH") == 0) {
+            search(key, m);
+        }
+    }
+
+    return 0;
+}
